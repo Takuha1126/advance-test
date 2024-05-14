2e3f4a5ab1683b3fad7b4160cb3c0315668493db
watanabetakuha <taku.neji1126@icloud.com>
2024-05-14 18:38:26 +0900

    cssの修正

--- src/resources/views/emails/reservation_confirmed.blade.php
+++ src/resources/views/emails/reservation_confirmed.blade.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>予約確認</title>
 </head>
 <body>
