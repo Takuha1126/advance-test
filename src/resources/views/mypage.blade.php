69b3f1a0f8c38f57074caaad8a35378f06c9cc85
watanabetakuha <taku.neji1126@icloud.com>
2024-05-17 12:54:03 +0900

    マイページに評価ボタン追加

--- src/resources/views/mypage.blade.php
+++ src/resources/views/mypage.blade.php
@@ -131,6 +131,10 @@
                                         <form action="{{ route('detail', ['shop_id' => $shop->id]) }}" method="GET">
                                             <button class="button__title" type="submit">詳しく見る</button>
                                         </form>
+                                        <form action="{{ route('evaluation.show', ['shopId' => $shop->id]) }}" method="GET">
+                                            @csrf
+                                            <button class="button__title" data-shop-id="{{ $shop->id }}">評価</button>
+                                        </form>
                                         @if ($favorite->status)
                                         <form action="{{ route('favorite.toggle', ['shopId' => $shop->id]) }}" method="post">
                                             @csrf
