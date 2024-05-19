0112f08d10ffa0a5a6ff0d56c216f0fc25fbed66
watanabetakuha <taku.neji1126@icloud.com>
2024-05-19 18:53:10 +0900

    ミドルウェアの不要なコード削除

--- src/app/Models/Admin.php
+++ src/app/Models/Admin.php
@@ -6,11 +6,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+
 class Admin extends Authenticatable
 {
     use HasFactory;
-     protected $table = 'admins';
-     protected $fillable = [
+
+    protected $fillable = [
         'admin_name',
         'email',
         'password',
