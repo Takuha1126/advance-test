273f69c1e169d080f68813779dea165cfa3ee102
watanabetakuha <taku.neji1126@icloud.com>
2024-05-19 23:56:47 +0900

    inputの幅修正

--- src/resources/views/admin/verify.blade.php
+++ src/resources/views/admin/verify.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>メール確認</title>
     <link rel="stylesheet" href="{{ asset('css/admin/registermail.css')}}">
 </head>
 <body>
