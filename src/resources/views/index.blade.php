f282a37cb06d1b5d8ae013df5ddabc08c9b3da4c
watanabetakuha <taku.neji1126@icloud.com>
2024-05-21 14:09:25 +0900

    不要なコード削除

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -121,8 +121,7 @@
         @endif
     </main>
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js"></script>
+
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js"></script>
 <script>
