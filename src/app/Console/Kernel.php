c5b1f2ba04c355cd8c2541ea8a5d8300284bd4cd
watanabetakuha <taku.neji1126@icloud.com>
2024-05-21 11:02:05 +0900

    リマインダーの修正

--- src/app/Console/Kernel.php
+++ src/app/Console/Kernel.php
@@ -17,7 +17,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('send:reservation-reminders')->dailyAt('10:57');
+        $schedule->command('send:reservation-reminders')->dailyAt('11:04');
         $schedule->command('reservations:delete-expired')->everyMinute();
     }
 
